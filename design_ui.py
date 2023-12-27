74b98ea48d67c7a8b139571934e9b132093d6c39
xyzaraa <kiarazzahraaa@gmail.com>
2023-12-27 08:57:52 +0700

    first push

---
+++ design_ui.py
@@ -0,0 +1,64 @@
+SIZE = 400
+GRID_LEN = 4
+GRID_PADDING = 10
+
+BACKGROUND_COLOR_GAME = "#60A3D9"
+BACKGROUND_COLOR_CELL_EMPTY = "#BFD7ED"
+
+BACKGROUND_COLOR_DICT = {
+2:      "#E9EAEC",
+4:      "#D5D5D5",
+8:      "#BEBEBE",
+16:     "#A9A9A9",
+32:     "#9E9E9E",
+64:     "#5D5D5D",
+128:    "#edcf72",
+256:    "#EFBE00",
+512:    "#333652",
+1024:   "#4B4F76",
+2048:   "#B6BBEA",
+4096:   "#eee4da",
+8192:   "#8F98ED",
+16384:  "#767DBC",
+32768:  "#EA8E04",
+65536:  "#7A016E",
+}
+
+CELL_COLOR_DICT = {
+2:      "#000000",
+4:      "#000000",
+8:      "#FFFFFF",
+16:     "#FFFFFF",
+32:     "#FFFFFF",
+64:     "#FFFFFF",
+128:    "#333652",
+256:    "#333652",
+512:    "#E9E5E5",
+1024:   "#FFFFFF",
+2048:   "#FFFFFF",
+4096:   "#776e65",
+8192:   "#481FA0",
+16384:  "#F9F9F9",
+32768:  "#3E3E43",
+65536:  "#FFFFFF",
+}
+
+FONT = ("Irish Grover",40,"bold")
+
+KEY_QUIT = "Escape"
+KEY_BACK = "b"
+
+KEY_UP = "Up"
+KEY_DOWN = "Down"
+KEY_LEFT = "Left"
+KEY_RIGHT = "Right"
+
+KEY_UP_ALT1 = "w"
+KEY_DOWN_ALT1 = "s"
+KEY_LEFT_ALT1 = "a"
+KEY_RIGHT_ALT1 = "d"
+
+KEY_UP_ALT2 = "i"
+KEY_DOWN_ALT2 = "k"
+KEY_LEFT_ALT2 = "j"
+KEY_RIGHT_ALT2 = "l"
